f4ec4e7219b4455188146cfee681078a005f1fc3
Gabriel da Fonseca Meira <gabriel.gfm@outlook.com>
2025-01-16 20:38:20 -0300

    data insert scripts

---
+++ src/main/resources/db/migration/default/V3__insert_skills_on_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO skills (nome) VALUES ('Java');
+INSERT INTO skills (nome) VALUES ('JavaScript');
+INSERT INTO skills (nome) VALUES ('Docker');
+INSERT INTO skills (nome) VALUES ('PHP');
+INSERT INTO skills (nome) VALUES ('C#');
+INSERT INTO skills (nome) VALUES ('C++');
+INSERT INTO skills (nome) VALUES ('GoLang');
